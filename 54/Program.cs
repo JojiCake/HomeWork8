4fe432da0b9dd43fde80f3cc9ee62f8be0b0d347
egor-turaev <egor-turaev@mail.ru>
2022-09-05 17:29:02 +0300

    saveWork

---
+++ 54/Program.cs
@@ -0,0 +1,65 @@
+﻿void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CreateArrayWhithRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int ReadInputData(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+
+
+
+Console.Clear();
+Console.WriteLine($"Введите размер массива m x n и диапазон случайных значений:");
+int m = ReadInputData("Введите m: ");
+int n = ReadInputData("Введите n: ");
+
+int[,] array = new int[m, n];
+CreateArrayWhithRandomNumbers(array);
+PrintArray(array);
+
+Console.WriteLine($"Отсортированный массив: ");
+OrderArrayLines(array);
+PrintArray(array);
